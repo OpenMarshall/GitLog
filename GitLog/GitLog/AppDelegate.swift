cc437e9b68696287deb3d47f9bc7ecc1134d75a7
KyXu <wm-ing@qq.com>
2017-07-14 18:00:06 +0800

    UI implemented

---
+++ GitLog/GitLog/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitLog
+//
+//  Created by KyXu on 2017/7/14.
+//  Copyright © 2017年 KyXu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitLog/GitLog/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitLog
+//
+//  Created by KyXu on 2017/7/14.
+//  Copyright © 2017年 KyXu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
