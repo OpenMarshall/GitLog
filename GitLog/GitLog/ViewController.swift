336b677207fd3108d5c4b4786405aafa5f69eab5
KyXu <wm-ing@qq.com>
2017-07-14 20:14:54 +0800

    git log success

--- GitLog/GitLog/ViewController.swift
+++ GitLog/GitLog/ViewController.swift
@@ -10,10 +10,13 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var projDirField: NSTextField!
+    @IBOutlet var authorNameField: NSTextField!
+    @IBOutlet var consoleTextView: NSTextView!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -21,7 +24,57 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    
+    // MARK: - Shell
+    func shell(launchPath: String, arguments: [String]) -> String
+    {
+        let task = Process()
+        task.launchPath = launchPath
+        task.arguments = arguments
+        task.currentDirectoryPath = projDirField.stringValue
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: String.Encoding.utf8)!
+        if output.characters.count > 0 {
+            //remove newline character.
+            let lastIndex = output.index(before: output.endIndex)
+            return output[output.startIndex ..< lastIndex]
+        }
+        return output
+    }
+    
+    func bash(command: String, arguments: [String]) -> String {
+        let whichPathForCommand = shell(launchPath: "/bin/bash", arguments: [ "-l", "-c", "which \(command)" ])
+        return shell(launchPath: whichPathForCommand, arguments: arguments)
+    }
+    
+    // MARK: - IBAction
+    @IBAction func browseDir(_ sender: Any) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseFiles = true
+        openPanel.begin { (result) in
+            if result == NSFileHandlingPanelOKButton, let path = openPanel.url?.path {
+                self.projDirField.stringValue = path
+            }
+        }
+    }
+    
+    
+    @IBAction func generate(_ sender: Any) {
+        guard projDirField.stringValue.characters.count != 0 else {
+            return
+        }
+        let log = bash(command: "git", arguments: ["log","--since","'5 days ago'","--oneline",
+                                                   "--author",authorNameField.stringValue])
+        print(log)
+    }
 }
 
